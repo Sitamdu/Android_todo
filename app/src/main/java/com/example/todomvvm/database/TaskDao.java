44cfb465f6605733e4450f783728511517cf0a9f
Sitamdu <beekramrai22@gmail.com>
2020-06-11 11:40:49 +0545

    dao

---
+++ app/src/main/java/com/example/todomvvm/database/TaskDao.java
@@ -0,0 +1,29 @@
+package com.example.todomvvm.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+@Dao
+// TODO (01) Create annotated interface TaskDao.
+public interface TaskDao {
+
+    @Query("select * from task order by priority")
+    List<TaskEntry> loadAllTasks();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertTask(TaskEntry task);
+
+    @Update
+    void update(TaskEntry task);
+
+    @Delete
+    void deleteTask(TaskEntry task);
+
+
+
+}
